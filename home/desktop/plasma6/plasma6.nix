59850f5a7f35602aef8f11509d85b6f78cc4f596
Joshua Nestler <development@nekanu.com>
2024-08-14 12:27:49 +0200

    Add PowerOptions

--- home/desktop/plasma6/plasma6.nix
+++ home/desktop/plasma6/plasma6.nix
@@ -12,6 +12,34 @@
       clickItemTo = "select";
     };
 
+    powerdevil.AC = {
+      powerButtonAction = "sleep";
+      autoSuspend = {
+        action = "nothing";
+        # idleTimeout = 900;
+      };
+      turnOffDisplay = {
+        idleTimeout = "never";
+        # idleTimeoutWhenLocked = "immediately";
+      };
+
+      whenLaptopLidClosed = "sleep";
+    };
+
+    powerdevil.battery = {
+      powerButtonAction = "sleep";
+      autoSuspend = {
+        action = null; # Sleep
+        idleTimeout = 900;
+      };
+      turnOffDisplay = {
+        idleTimeout = 900;
+        idleTimeoutWhenLocked = "immediately";
+      };
+
+      whenLaptopLidClosed = "sleep";
+    };
+
     # Enable NumLock on startup
     configFile."kcminputrc".Keyboard.NumLock.value = 0;
   };
