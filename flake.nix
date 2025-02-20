4c44448f57c7ea80b66b7ee1578d507e8bab8a98
Nekanu <development@nekanu.com>
2025-02-20 12:37:31 +0100

    remove broken nixos generator until everything else works

--- flake.nix
+++ flake.nix
@@ -97,11 +97,6 @@
       # Your custom packages and modifications, exported as overlays
       overlays = import ./overlays { inherit nixpkgs inputs; };
 
-      install-iso = inputs.nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        mo
-      };
-
       # NixOS configuration entrypoint
       # Available through 'nixos-rebuild --flake .#your-hostname'
       nixosConfigurations = {
