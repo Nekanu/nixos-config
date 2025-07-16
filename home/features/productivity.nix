01b78b4da33b152da57694ace16b2d7fbb994361
Nekanu <development@nekanu.com>
2025-07-16 19:19:05 +0200

    Use native KeepassXC package

--- home/features/productivity.nix
+++ home/features/productivity.nix
@@ -3,6 +3,7 @@
   home.packages = with pkgs.unstable; [
     tor-browser-bundle-bin
     libreoffice
+    keepassxc
   ];
 
   services.nextcloud-client = {
@@ -18,7 +19,7 @@
         origin = "flathub-beta";
       } # Email client (Beta due to Tray support)
       # "com.ulduzsoft.Birdtray" # Thunderbird tray icon
-      "org.keepassxc.KeePassXC" # Password manager
+      #"org.keepassxc.KeePassXC" # Password manager
       "com.usebottles.bottles" # Wine compatibility layer
       "com.logseq.Logseq" # Note-taking app
       "org.onlyoffice.desktopeditors" # Office suite
@@ -36,6 +37,7 @@
         filesystems = [
           "home:rw" # Allow access to home directory
           "/run/media/${username}:rw" # Allow access to removable media
+          "/mnt/Data1:rw" # Allow access to Data1 disk
         ];
       };
     };
