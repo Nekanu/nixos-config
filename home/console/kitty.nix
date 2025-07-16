c175ab63aa59e5fea52715e3f2ee0fca08e13fde
Nekanu <development@nekanu.com>
2025-07-16 19:30:43 +0200

    Use program config for Kitty

--- home/console/kitty.nix
+++ home/console/kitty.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    kitty
-  ];
+  programs.kitty = {
+    enable = true;
+    package = pkgs.kitty;
+  };
 
   # home.file.".config/kitty/kitty.conf".source = ./kitty.conf;
 }
