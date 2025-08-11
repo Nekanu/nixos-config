68f561c7ec808dfd701e504061e04ccc86feac38
Nekanu <development@nekanu.com>
2025-08-11 11:30:21 +0200

    Move distrobox to own file

---
+++ home/features/distrobox.nix
@@ -0,0 +1,26 @@
+{ ... }:
+{
+  programs.distrobox = {
+    enable = true;
+    enableSystemdUnit = false;
+    # containers = {
+    #   ubuntu = {
+    #     image = "ubuntu:latest";
+    #     entry = true;
+    #     init_hooks = [
+    #       "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker"
+    #       "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker-compose"
+    #     ];
+    #   };
+
+    #   arch = {
+    #     image = "archlinux:latest";
+    #     entry = true;
+    #     init_hooks = [
+    #       "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker"
+    #       "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker-compose"
+    #     ];
+    #   };
+    # };
+  };
+}
