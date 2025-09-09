1fdde5e9a2c449026b32277e34d3b6550ce8ccb1
Nekanu <development@nekanu.com>
2025-09-09 10:51:08 +0200

    Declare hyprland packages in default.nix

--- home/desktop/hyprland/hyprland.nix
+++ home/desktop/hyprland/hyprland.nix
@@ -1,27 +1,5 @@
 { lib, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    # Terminal
-    kitty
-    # File Manager
-    xfce.thunar
-
-    #xdg-desktop-portal-hyprland
-
-    hyprpaper # wallpaper
-    hyprdim # dim inactive windows
-    hyprshot # screenshot
-    hyprlock # lockscreen
-    hyprkeys # keybinds
-    hypridle # idle daemon
-    hyprpicker # color picker
-    hyprcursor # cursor theme
-    hyprland-workspaces # workspaces
-
-    grimblast # screenshot
-    cliphist # clipboard
-  ];
-
   wayland.windowManager.hyprland = {
     enable = true;
 
